29a4451cbcd2baadffd8dab2e387d34146cbc136
Frued Dued <thatfrueddued@github.com>
2020-09-12 15:58:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ wsServer.on('request', function(request) {
     }), 'base64'));
     connection.on('message', function(encmessage) {
       let aes = new aesjs.ModeOfOperation.cbc(key, iv);
-      let message = aes.decrypt(Buffer.from(encmessage.utf8Data, 'base64').buffer);
+      let message = aes.decrypt(Buffer.from(encmessage.utf8Data, 'base64'));
       if (encmessage.type === 'utf8') {
         try {
           console.log('Received Message: ' + message);
