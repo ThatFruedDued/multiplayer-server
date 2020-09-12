4594e1512fb9cf4cb81d5cc7583ce24bbcb4600b
Frued Dued <thatfrueddued@github.com>
2020-09-12 15:15:31 -0400

    Use AES
    Start using AES cipher (after initial RSA cipher to establish client) for additional speed and anticheat measures.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 let WebSocketServer = require('websocket').server;
 let http = require('http');
 let NodeRSA = require('node-rsa');
+let aesjs = require('aes-js');
+let crypto = require('crypto');
 
-const key = new NodeRSA("-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAor8nQt6VVNClBUQmaNeZNeUCcfZ4IeLV1sUACZ5/fcfrgycp9ew3KtWOiHXqlFSGQp3qgMi5bhUDaGn89WqIDTfdgrTB1mIgkncWvx5H1J7ODYf7QzFalMMjdpaSbDHsDACvOAWFoBtruOR6hOH337WaNLGNZd6kbnG4wGK86PJqE0pVaYo74WzgVe+8YXM/NTCgTQy0TplzMTCkBFM9wyAU7bONuMZoggp65nvYOfvTqk3YtxUzgbIg4BfORnTwzC2UraXQTlbFzJjhsguAQ0OG2iNA+ZHjzI2uSepunlyP8woxKDliyfiqsDW24ynIjdooLJV5KqJhDHHsQZOuWQIDAQAB -----END PUBLIC KEY-----");
+const rsakey = new NodeRSA("-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAor8nQt6VVNClBUQmaNeZNeUCcfZ4IeLV1sUACZ5/fcfrgycp9ew3KtWOiHXqlFSGQp3qgMi5bhUDaGn89WqIDTfdgrTB1mIgkncWvx5H1J7ODYf7QzFalMMjdpaSbDHsDACvOAWFoBtruOR6hOH337WaNLGNZd6kbnG4wGK86PJqE0pVaYo74WzgVe+8YXM/NTCgTQy0TplzMTCkBFM9wyAU7bONuMZoggp65nvYOfvTqk3YtxUzgbIg4BfORnTwzC2UraXQTlbFzJjhsguAQ0OG2iNA+ZHjzI2uSepunlyP8woxKDliyfiqsDW24ynIjdooLJV5KqJhDHHsQZOuWQIDAQAB -----END PUBLIC KEY-----");
 
 let server = http.createServer(function(request, response) {
   console.log((new Date()) + ' Received request for ' + request.url);
@@ -35,19 +37,32 @@ wsServer.on('request', function(request) {
   try {
     connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
+    let key = crypto.randomBytes(32);
+    let iv = crypto.randomBytes(16);
     let verifyCode = Math.floor(Math.random() * 2147483647);
-    connection.sendUTF(key.encrypt(JSON.stringify({"VERIFY": verifyCode}), 'base64'));
-    connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-        console.log('Received Message: ' + message.utf8Data);
-        if(JSON.parse(message.utf8Data).VERIFY = verifyCode) {
-          console.log(JSON.parse(message.utf8Data).POS);
-          gnvc();
-          connection.sendUTF(key.encrypt(JSON.stringify({
-            "VERIFY": verifyCode
-          }), 'base64'));
-        } else {
-          connection.sendUTF("Connection unverifiable.");
+    connection.sendUTF(rsakey.encrypt(JSON.stringify({
+      "VERIFY": verifyCode,
+      "KEY": key.toString("base64"),
+      "IV": iv.toString("base64");
+    }), 'base64'));
+    connection.on('message', function(encmessage) {
+      let aes = new aesjs.modeOfOperation.cbc(key, iv);
+      let message = aes.decrypt(Buffer.from(encmessage.utf8Data, 'base64').toString('utf8').buffer);
+      if (encmessage.type === 'utf8') {
+        try {
+          console.log('Received Message: ' + message);
+          if(JSON.parse(message).VERIFY = verifyCode) {
+            console.log(JSON.parse(message).POS);
+            aes = new aesjs.modeOfOperation.cbc(key, iv);
+            gnvc();
+            connection.sendUTF(aes.encrypt(JSON.stringify({
+              "VERIFY": verifyCode
+            }), 'base64'));
+          } else {
+            connection.sendUTF("Connection unverifiable.");
+          }
+        } catch (err) {
+          console.error("parsing error");
         }
       }
     });
